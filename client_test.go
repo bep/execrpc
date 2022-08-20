43c96c78cb1dacb1fbd5a6282c4ca279702cc8ea
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2022-08-20 16:08:13 +0200

    Make Header.Version/Status uint16
    One byte is a little on the small side, better changing this now before people start using this.

--- client_test.go
+++ client_test.go
@@ -143,6 +143,9 @@ func TestExecTyped(t *testing.T) {
 		_, err = client.Execute(model.ExampleRequest{Text: "world"})
 		c.Assert(err, qt.IsNil)
 		c.Assert(len(logMessages), qt.Equals, 2)
+		c.Assert(string(logMessages[0].Body), qt.Equals, "first log message")
+		c.Assert(logMessages[0].Header.Status, qt.Equals, uint16(0))
+		c.Assert(logMessages[0].Header.Version, qt.Equals, uint16(32))
 		c.Assert(client.Close(), qt.IsNil)
 	})
 
